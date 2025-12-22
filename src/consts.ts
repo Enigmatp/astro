7a25702fb11dcbbf0cbf36653a2095d98455c67e
wuf <wuf@nowoce.com>
2025-12-22 09:46:49 +0800

    Test2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "洋洋小窝";
-export const SITE_DESCRIPTION = "洋洋的日常";
+export const SITE_TITLE = "YangYang's Eternal Flame";
+export const SITE_DESCRIPTION = "Behold the epic saga of Yangyang";
