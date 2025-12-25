5070ab742b8431ae927296ef4f8265aaf025afcb
wuf <wuf@nowoce.com>
2025-12-25 14:26:34 +0800

    Change

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "YangYang's Eternal Flame";
-export const SITE_DESCRIPTION = "Behold the epic saga of Yangyang";
+export const SITE_TITLE = "Enigma";
+export const SITE_DESCRIPTION = "Enigma's Personal Blog";
