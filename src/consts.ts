b91a92bb48be051a3c347166dadf5de2bd476970
wuf <wuf@nowoce.com>
2025-12-25 14:38:37 +0800

    Change constant

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Enigma";
-export const SITE_DESCRIPTION = "Enigma's Personal Blog";
+export const SITE_TITLE = "小谜团";
+export const SITE_DESCRIPTION = "小谜团的日常";
