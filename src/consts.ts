9220025b8b74ea859aacfffd9c6d29a9fa1dfa14
wuf <wuf@nowoce.com>
2025-12-29 10:39:55 +0800

    Fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "圣光忽悠着你";
-export const SITE_DESCRIPTION = "圣光的日常";
+export const SITE_TITLE = "圣光忽悠";
+export const SITE_DESCRIPTION = "圣光忽悠";
